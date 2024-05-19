d86c8246c5303d116dc1b994e65c5ae3183612e4
PTQuality <kontakt.tunskipiotr@gmail.com>
2024-05-19 19:08:17 +0200

    Finished second module

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,6 +11,8 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  timeout: 10 * 1000,
+
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -24,10 +26,11 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'https://demo-bank.vercel.app',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,32 +1,47 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
+import { loginData } from '../test-data/login.data';
 
-test.describe("User login to Demobank", () => {
-  test("successful login with correct credentials", async ({ page }) => {
-    await page.goto("https://demo-bank.vercel.app/");
-    await page.getByTestId("login-input").fill("testerLO");
-    await page.getByTestId("password-input").fill("12345678");
-    await page.getByTestId("login-button").click();
+test.describe('User login to Demobank', () => {
+  const userLogin = loginData.userLogin;
+  const userPassword = loginData.userPassword;
 
-    await expect(page.getByTestId("user-name")).toHaveText("Jan Demobankowy");
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
+  test('successful login with correct credentials', async ({ page }) => {
+    //Arrange
+    const expectedUsername = 'Jan Demobankowy';
+    //Act
+    await page.getByTestId('login-input').fill(userLogin);
+    await page.getByTestId('password-input').fill(userPassword);
+    await page.getByTestId('login-button').click();
+
+    //Assert
+    await expect(page.getByTestId('user-name')).toHaveText(expectedUsername);
   });
 
-  test("unsuccessful login with to short username", async ({ page }) => {
-    await page.goto("https://demo-bank.vercel.app/");
-    await page.getByTestId("login-input").fill("tester");
-    await page.getByTestId("login-input").blur();
-    await expect(page.getByTestId("error-login-id")).toHaveText(
-      "identyfikator ma min. 8 znaków"
+  test('unsuccessful login with to short username', async ({ page }) => {
+    const tooShortUserLogin = 'tester';
+    const expectedLoginErrorMsg = 'identyfikator ma min. 8 znaków';
+
+    await page.getByTestId('login-input').fill(tooShortUserLogin);
+    await page.getByTestId('login-input').blur();
+
+    await expect(page.getByTestId('error-login-id')).toHaveText(
+      expectedLoginErrorMsg,
     );
   });
 
-  test("unsuccessful login with to short password", async ({ page }) => {
-    await page.goto("https://demo-bank.vercel.app/");
-    await page.getByTestId("login-input").fill("tester");
-    await page.getByTestId("password-input").fill("12345");
-    await page.getByTestId("password-input").blur();
+  test('unsuccessful login with to short password', async ({ page }) => {
+    const tooShortUserPassword = '123456';
+    const expectedPasswordErrorMsge = 'hasło ma min. 8 znaków';
 
-    await expect(page.getByTestId("error-login-password")).toHaveText(
-      "hasło ma min. 8 znaków"
+    await page.getByTestId('login-input').fill(userLogin);
+    await page.getByTestId('password-input').fill(tooShortUserPassword);
+    await page.getByTestId('password-input').blur();
+
+    await expect(page.getByTestId('error-login-password')).toHaveText(
+      expectedPasswordErrorMsge,
     );
   });
 });
